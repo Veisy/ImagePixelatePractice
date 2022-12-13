2914ef174706a7b551b8891ca7193e38a22f2f23
Veisy <knutveyshamsun@gmail.com>
2022-12-13 03:08:54 +0300

    Lung segmentation updated. Experimental works added.

--- app/src/main/java/com/vyy/imageprocessingpractice/processes/LungSegmentation.kt
+++ app/src/main/java/com/vyy/imageprocessingpractice/processes/LungSegmentation.kt
@@ -5,6 +5,8 @@ import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import androidx.core.graphics.drawable.toDrawable
 import com.vyy.imageprocessingpractice.utils.lastProcessTime
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 var topmostPixelIndex: Int = 0
 var leftmostPixelIndex: Int = 0
@@ -15,18 +17,9 @@ fun lungOperations(originalBitmap: Bitmap, resources: Resources): List<BitmapDra
 
     val a = originalBitmap.toDrawable(resources)
 
-    val b = highlightCorners(a.bitmap, resources)
+    highlightCorners(a.bitmap, resources)
 
-    val c = crop(
-        b.bitmap,
-        fromX = topmostPixelIndex,
-        fromY = leftmostPixelIndex,
-        toX = bottommostPixelIndex,
-        toY = rightmostPixelIndex,
-        resources
-    )
-
-    val d = crop(
+    val b = crop(
         a.bitmap,
         fromX = topmostPixelIndex,
         fromY = leftmostPixelIndex,
@@ -35,12 +28,28 @@ fun lungOperations(originalBitmap: Bitmap, resources: Resources): List<BitmapDra
         resources
     )
 
-    val e = crop(d.bitmap, fromX = (d.bitmap.width/7.5).toInt(), fromY = (d.bitmap.height/10), toX = (d.bitmap.width/1.15).toInt(), toY = (d.bitmap.height/1.4).toInt(), resources)
+    val c = crop(
+        b.bitmap,
+        fromX = (b.bitmap.width / 7.5).toInt(),
+        fromY = (b.bitmap.height / 10),
+        toX = (b.bitmap.width / 1.15).toInt(),
+        toY = (b.bitmap.height / 1.4).toInt(),
+        resources
+    )
+
+    val d = lungOperation1(c.bitmap, resources)
+    val e = lungOperation2(d.bitmap, resources)
     return listOf(b, c, d, e)
 }
 
 private fun highlightCorners(bitmap: Bitmap, resources: Resources): BitmapDrawable =
     applySpecialFilter(bitmap, resources, ::getHighlightedOrBlackPixel)
+
+private fun lungOperation1(bitmap: Bitmap, resources: Resources): BitmapDrawable =
+    applySpecialFilter(bitmap, resources, ::operation1)
+
+private fun lungOperation2(bitmap: Bitmap, resources: Resources): BitmapDrawable =
+    applySpecialFilter(bitmap, resources, ::operation2)
 
 // Take filter function as parameter and apply it to each pixel.
 private fun applySpecialFilter(
@@ -109,3 +118,59 @@ private fun getHighlightedOrBlackPixel(pixels: IntArray, width: Int, x: Int, y: 
 
     return 0xff000000.toInt() or (pixelColor shl 16) or (pixelColor shl 8) or pixelColor
 }
+
+private fun operation1(pixels: IntArray, width: Int, x: Int, y: Int): Int {
+    val differenceThreshold = 125
+    val nearbyPixels = IntArray(441)
+
+    var squareIndex = 0
+    for (i in -10..10) {
+        for (j in -10..10) {
+            val index = (y + i) * width + (x + j)
+            if (index < 0 || index >= pixels.size) continue
+            val pixel = pixels[index]
+            val red = pixel shr 16 and 0xff
+            val green = pixel shr 8 and 0xff
+            val blue = pixel and 0xff
+            nearbyPixels[squareIndex] = (red + green + blue) / 3
+            squareIndex++
+        }
+    }
+
+    val pixelColor: Int =
+        if ((nearbyPixels.max() - nearbyPixels.min()) < differenceThreshold && nearbyPixels.average() > 175) {
+            200
+        } else {
+            pixels[y * width + x]
+        }
+
+    return 0xff000000.toInt() or (pixelColor shl 16) or (pixelColor shl 8) or pixelColor
+}
+
+private fun operation2(pixels: IntArray, width: Int, x: Int, y: Int): Int {
+
+    // pixel color
+    val pixel = pixels[y * width + x]
+    val red = pixel shr 16 and 0xff
+    val green = pixel shr 8 and 0xff
+    val blue = pixel and 0xff
+
+    val pixelOldColor = (red + green + blue) / 3
+
+    val pixelNewColor: Int = if (pixelOldColor < 100
+        && (getDistanceToCenter(x, y, width, pixels.size / width) > width / 2)
+    ) {
+        200
+    } else {
+        pixelOldColor
+    }
+
+    return 0xff000000.toInt() or (pixelNewColor shl 16) or (pixelNewColor shl 8) or pixelNewColor
+}
+
+private fun getDistanceToCenter(x: Int, y: Int, width: Int, height: Int): Double {
+    val centerX = width / 2.0
+    val centerY = height / 2.0
+    return sqrt((x - centerX).pow(2) + (y - centerY).pow(2))
+}
+
